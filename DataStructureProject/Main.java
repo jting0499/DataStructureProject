c9b83d748fa54292409fdf33efba74eb71f0bb23
Tinclaire <93721254+Tinclaire@users.noreply.github.com>
2023-01-03 21:12:54 +0800

    Update Main.java
    tintin update - can new WebPage

--- DataStructureProject/Main.java
+++ DataStructureProject/Main.java
@@ -16,6 +16,24 @@ public class Main {
 		String input = sc.next();
 		File file = new File("keywordList.txt");
 		Scanner scanner = new Scanner(file);
+		
+		GoogleQuery g = new GoogleQuery(input);
+		try {
+			g.query();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		ArrayList<WebPage> webs = new ArrayList<>();
+		for(int i = 0; i < g.titles.size(); i++) {
+			WebPage w = new WebPage(g.urls.get(i), g.titles.get(i));
+			webs.add(w);
+		}
+		for(WebPage w : webs) {
+			System.out.println(w.name);
+		}
+		System.out.println(g.titles);
 
 		while(scanner.hasNextLine()){
 //			int numOfKeywords = scanner.nextInt();
@@ -38,18 +56,30 @@ public class Main {
 		scanner.close();
 		
 		try {
-			FileWriter writer = new FileWriter("url.txt");
-			GoogleQuery g = new GoogleQuery(input);
-			writer.write(g.query().toString());
-			writer.close();
-			System.out.println("done");
-			System.out.println(g.titles);
-			System.out.println(g.urls);
-			sc.close();
-		} catch (IOException e1) {
+			for(WebPage w : webs) {
+				w.setScore(keywords);
+				System.out.println(w.score);
+			}
+//			test.setScore(keywords);
+//			System.out.println(test.score);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
 		}
+		
+// 		try {
+// 			FileWriter writer = new FileWriter("url.txt");
+// 			GoogleQuery g = new GoogleQuery(input);
+// 			writer.write(g.query().toString());
+// 			writer.close();
+// 			System.out.println("done");
+// 			System.out.println(g.titles);
+// 			System.out.println(g.urls);
+// 			sc.close();
+// 		} catch (IOException e1) {
+// 			// TODO Auto-generated catch block
+// 			e1.printStackTrace();
+// 		}
 		
 //		try 
 //		{
